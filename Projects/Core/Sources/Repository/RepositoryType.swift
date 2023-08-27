4d89575a6debf7bd72a52e106ef1a5dec7691783
ksc89kim <ksc89kim@naver.com>
2023-08-27 22:12:12 +0900

    Core > Repository >  RepositoryType 추가

---
+++ Projects/Core/Sources/Repository/RepositoryType.swift
@@ -0,0 +1,12 @@
+//
+//  RepositoryType.swift
+//  Core
+//
+//  Created by kim sunchul on 2023/08/27.
+//  Copyright © 2023 com.tronplay. All rights reserved.
+//
+
+import Foundation
+
+public protocol Repository {
+}
